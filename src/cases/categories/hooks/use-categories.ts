1dc2fdb9c7d831aa7ffd865cad36202ec0088251
vito1309 <vitornegri2005@gmail.com>
2025-10-13 21:47:11 -0300

    Implementando Case Category (Aula 13/10)

---
+++ src/cases/categories/hooks/use-categories.ts
@@ -0,0 +1,41 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { CateogryService } from "../services/category.service";
+import type { CategoryDTO } from "../dtos/category.dto";
+
+
+export function useCategories(){ //hook que retorna todos os dados das categories
+    return useQuery<CategoryDTO[]>({ //o hook retorna o estado, além dos dados, coisa que a função sozinha não faz
+        queryKey: ['categories'], 
+        queryFn: CateogryService.list 
+    });
+}
+
+export function useCategory(id: string){ //retorna apenas os IDs
+    return useQuery<CategoryDTO>({
+        queryKey:['category', id],
+        queryFn: () => CateogryService.getById(id),
+        enabled: !id 
+        //O uso do '!' indica a negação de uma informação, assim deixando habilitado que categorias que não possuam ID sejam apresentadas
+        //Ao utilizar '!!' a negação informada anteriormente é negada, assim apresentado somente as categorias com ID
+        //...ao realizar a busca pelos IDs e for identificado IDs iguais aos carregados anteriormente, 
+        //..esta informação é puxada do cache
+    });    
+}
+
+export function userCreateCategory(){
+    return useMutation<CategoryDTO, Error, Omit<CategoryDTO, 'id'>>({//A informação ID será omitida caso de um erro
+        mutationFn: (category: Omit<CategoryDTO, 'id'>) => CateogryService.create(category);
+    });
+}
+
+export function userUpdateCategory(){
+    return useMutation<CategoryDTO, Error, {id: string, category: CategoryDTO}>({
+        mutationFn: ({id, category}) => CateogryService.update(id, category)
+    });
+}
+
+export function userDeleteCategory(){
+    return useMutation<void, Error, string>({//<void> não traz informações
+        mutationFn: (id: string) => CateogryService.delete(id)
+    });
+}
