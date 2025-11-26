50f43b01733d9fe796b7a44ca7aa41e64437e8e4
vito1309 <vitornegri2005@gmail.com>
2025-11-26 04:06:32 -0300

    Ajustes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,10 @@
-//ao criar um arquivo com um nome separado por uma '/' é possível criar uma pasta com o nome informado antes da barra, assim como este arquivo está
 
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, //Com a configuração de ambiente no arquivo '.env' a url real não é mostrada
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json' 
-        //o sistema apenas irá se comunicar a URL (http://...) com um conteúdo (Content-Type) do tipo json
     }
 });
 
